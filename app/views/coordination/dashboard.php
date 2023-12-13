7800ed3abbc2446cdde67ad37bd3e255cd43fd59
Raph-source <20ik061@esisalama.org>
2023-12-13 21:27:39 +0200

    remise de bouton retour et de l'option changer mot de passe

--- app/views/coordination/dashboard.php
+++ app/views/coordination/dashboard.php
@@ -8,13 +8,15 @@
 <a href="lancer-les-votes">lancer les votes</a><br>
 <a href="publier-les-resultat">publlier les resultats</a><br>
 <a href="relancer-les-votes">relancer les elections</a><br>
+<a href="mot de passe">Changer le mot de passe</a><br>
+
 <?php
     echo 'coordination '. $coordination . '<br>';
     echo 'nombre candidature '. $nombreCandidature . '<br>';
     echo 'nombre voix ' . $nombreVoix . '<br>';
     echo 'voix gagnant '.$voixGagnant . '<br>';//qui permettra de faire la bare de progression 
     echo 'jourVote restant '.$jourVoteRestant . '<br>';
-?>
+?>  
 <?php if(count($resultat) > 0):?>
     <p>Resultat</p>
     <?php foreach($resultat as $value):?>
@@ -31,6 +33,18 @@
 
     </div>
 </div>
+
+<!-- le bouton retour-->
+<?php if(isset($_SESSION['idPromotion'])):?>
+    <?php if($_SESSION['idPromotion'] == 1):?>
+        <a href="retour?vers=authentification-coordination">Retour</a>
+    <?php else:?>
+        <a href="retour?vers=choix-groupe">Retour</a>
+    <?php endif?>
+<?php else:?>
+    <?php header("Location: retour?vers=authentification-coordination")?>
+<?php endif?>
+
 <?php
     require_once FOOTER;
 ?>
