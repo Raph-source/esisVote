f97a6fc59f280fd6e6a68dbb2c9bed223feb6c7c
Raph-source <20ik061@esisalama.org>
2024-01-29 17:10:37 +0200

    teste

--- app/models/Model.php
+++ app/models/Model.php
@@ -4,7 +4,7 @@ class Model{
 
     public function __construct(){
         //connexion à la bdd mysql
-        $this->bdd = new PDO("mysql:host=localhost;dbname=esisvote", "root", '');
+        $this->bdd = new PDO("mysql:host=localhost;dbname=esisvote", "raph", 'mk271319');
     }
 
     //cette méthode permet gérer l'authentification
