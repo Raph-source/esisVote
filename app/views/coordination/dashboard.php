30d27d8baeb57142bf0f3fc9e2aa0f04bb0e01fb
Raph-source <20ik061@esisalama.org>
2024-01-30 20:42:16 +0200

    gerer erreur

--- app/views/coordination/dashboard.php
+++ app/views/coordination/dashboard.php
@@ -81,7 +81,7 @@
             </svg>
             <span>Relancer les elections</span>
         </a>
-        <a href="mot de passe">
+        <a href="changermotdepasse">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-lock" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
@@ -198,7 +198,7 @@
                 </svg>
                 <span>Relancer les elections</span>
             </a>
-            <a href="mot-de-passe">
+            <a href="changermotdepasse">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-lock" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
