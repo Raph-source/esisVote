3b1b3a0750be85b90baa08dd2738ae52fc5b0477
Raph-source <20ik061@esisalama.org>
2024-02-04 17:09:58 +0200

    test

--- app/controllers/EtudiantController.php
+++ app/controllers/EtudiantController.php
@@ -277,7 +277,7 @@ class EtudiantController{
 
     public function voirProjet(){
         header('Content-Type: text/html; charset=utf-8');
-        
+                
         if($this->superGlobal->noEmptyGet(['projet'])){
             $idCandidature = intval($this->superGlobal->get['projet']);
             $projet = $this->model->candidature->getProjet($idCandidature);
